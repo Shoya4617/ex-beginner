c7376a85036ca2b4d1d8a75122a0ec030a04d04f
yamasekishoya <shoya.4617@gmail.com>
2020-02-04 15:22:18 +0900

    Exam04Controller,UserForm,Userを作成

--- src/main/java/com/example/Controller/Exam04Controller.java
+++ src/main/java/com/example/Controller/Exam04Controller.java
@@ -1,11 +1,19 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.UserForm;
 
 @Controller
 @RequestMapping("/04")
 public class Exam04Controller {
+	
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
 
 	@RequestMapping("")
 	public String index() {
